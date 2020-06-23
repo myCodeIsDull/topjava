d0f2dc4235110f02597b5c9db68ab814a9cc3faa
myCodeIsDull <myCodeIsDull@users.noreply.github.com>
2020-06-23 23:29:32 +0300

    HW3_initial_commit

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,12 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id, datetime) VALUES
+('Завтрак', 200, 100000, '2020-01-30 09:20:00'),
+('Обед', 800, 100000, '2020-01-30 13:00:00'),
+('Ужин', 1000, 100000, '2020-01-30 20:35:00'),
+('Еда на граничное значение', 50, 100000, '2020-01-31 00:00:00'),
+('Обед', 500, 100000, '2020-01-31 08:15:00'),
+('Ужин', 800, 100000, '2020-01-31 14:45:00')
+
