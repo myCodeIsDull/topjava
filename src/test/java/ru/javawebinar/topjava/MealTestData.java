21627cb8f1ed943238ba76698cfcb301319b2718
myCodeIsDull <myCodeIsDull@users.noreply.github.com>
2020-06-24 17:41:24 +0300

    Add some basic tests for JdbcMealRepository and test data for it

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,50 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+
+public class MealTestData {
+    public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
+    public static final int MEAL_ID = 100_003;
+    public static final Meal MEAL = new Meal(100_003, LocalDateTime.parse("2020-01-31 08:15:00", formatter), "Обед", 500);
+
+    public static final List<Meal> USER_MEALS = Arrays.asList(new Meal(LocalDateTime.parse("2020-01-31 14:45:00", formatter), "Ужин", 800),
+            new Meal(LocalDateTime.parse("2020-01-31 08:15:00", formatter), "Обед", 500),
+            new Meal(LocalDateTime.parse("2020-01-31 00:00:00", formatter), "Еда на граничное значение", 50),
+            new Meal(LocalDateTime.parse("2020-01-30 20:35:00", formatter), "Ужин", 1000),
+            new Meal(LocalDateTime.parse("2020-01-30 13:00:00", formatter), "Обед", 800),
+            new Meal(LocalDateTime.parse("2020-01-30 09:20:00", formatter), "Завтрак", 200));
+
+    public static final List<Meal> FILTERED_MEALS = Arrays.asList(new Meal(LocalDateTime.parse("2020-01-31 14:45:00", formatter), "Ужин", 800),
+            new Meal(LocalDateTime.parse("2020-01-31 08:15:00", formatter), "Обед", 500),
+            new Meal(LocalDateTime.parse("2020-01-31 00:00:00", formatter), "Еда на граничное значение", 50));
+
+    public static Meal getNew() {
+        return new Meal(LocalDateTime.now(ZoneId.systemDefault()), "test", 550);
+    }
+
+    public static Meal getUpdated() {
+        return new Meal(MEAL_ID, LocalDateTime.now(ZoneId.systemDefault()), "Updated", 830);
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("id").isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,79 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNull;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Autowired
+    private MealRepository repository;
+
+
+    @Test
+    public void get() {
+        Meal meal = service.get(MEAL_ID, USER_ID);
+        assertMatch(meal, MEAL);
+    }
+
+    @Test
+    public void delete() {
+        service.delete(MEAL_ID, USER_ID);
+        assertNull(repository.get(MEAL_ID, USER_ID));
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+        List<Meal> actual = service.getBetweenInclusive(parseLocalDate("2020-01-31"), parseLocalDate("2020-01-31"), USER_ID);
+        assertMatch(actual, FILTERED_MEALS);
+    }
+
+    @Test
+    public void getAll() {
+        List<Meal> all = service.getAll(USER_ID);
+        assertMatch(all, USER_MEALS);
+    }
+
+    @Test
+    public void update() {
+        Meal updated = getUpdated();
+        service.update(updated, USER_ID);
+        assertMatch(service.get(updated.getId(), USER_ID), updated);
+    }
+
+    @Test
+    public void create() {
+        Meal meal = getNew();
+        Meal createdMeal = service.create(meal, USER_ID);
+        int id = createdMeal.getId();
+        meal.setId(id);
+        assertMatch(createdMeal, meal);
+        assertMatch(service.get(id, USER_ID), meal);
+    }
+}
